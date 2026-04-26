3774efda1b69ca328581008959ac7a11d742573b
Nimisha Naik <154725910+nim-naik@users.noreply.github.com>
2026-04-26 22:39:36 +0530

    Refactor script.js for improved readability and structure
    Refactor script.js for improved readability and structure

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000";
+const API_BASE = "";
 let token = localStorage.getItem("token");
 document.getElementById("regUsername").value="";
 document.getElementById("regPassword").value="";
